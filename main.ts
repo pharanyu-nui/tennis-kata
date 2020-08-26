dd31b446749de21af5bde8b0f6f55aa8d0cedffb
pharanyu <pharanyu.c@codium.co>
2020-08-26 14:02:07 +0700

    complete all test case

--- main.ts
+++ main.ts
@@ -1,7 +1,3 @@
-//
-// Your implementations
-// 
-
 export interface Game {
     player1Score: number;
     player2Score: number;
@@ -16,7 +12,7 @@ export function updateGameScore(point1: number, point2: number): Game {
 
 export function decideGameWinner(game: Game): string {
     if (game.player1Score < 4 && game.player2Score < 4) {
-        return '??'
+        return 'No winner'
     }
 
     if (game.player1Score - game.player2Score >= 2) {
@@ -27,24 +23,21 @@ export function decideGameWinner(game: Game): string {
 }
 
 export function printScore(scores: number[]): string {
-    const textScore = ['love', 'fifteen', 'thirty', 'forty'];
-    const convertScoreToTexts: string[] = scores.map((score, index) => {
-        if (index !== scores.length - 1) {
-            return textScore[score] + ' - ' 
-        } else {
-            return textScore[score]
+    if (scores.length !== 2) {
+        return 'error';
+    }
+
+    if (scores[0] >= 3 && scores[1] >= 3) {
+        if (scores[0] === scores[1]) {
+            return 'deuce';
+        } else if (scores[0] - scores[1] === 1) {
+            return 'advantage player 1'
+        } else if (scores[1] - scores[0] === 1) {
+            return 'advantage player 2'
         }
-    });
-    return ''.concat(...convertScoreToTexts);
+    }
+
+    const textScore = ['love', 'fifteen', 'thirty', 'forty'];
+    
+    return `${textScore[scores[0]]} - ${textScore[scores[1]]}`;
 }
-
-
-// given
-const player1Points = 4;
-const player2Points = 0;
-
-// when
-const game = updateGameScore(player1Points, player2Points);
-const winner = decideGameWinner(game);
-
-console.log(winner);
